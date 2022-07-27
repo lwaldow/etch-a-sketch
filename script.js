d52d08c8c6368734833a6cae6cb3ddf0391848b8
Waldow <lwaldow2@gmail.com>
2022-07-27 09:32:24 -0700

    Add drawing functionality, fix draggable

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 const grid = document.querySelector("#grid");
 const absSize = 512;
-let gridSize = 32;
+let gridSize = 16;
+let currentColor = "black";
+
 grid.style.width = absSize + "px";
 grid.style.height = absSize + "px";
 grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
 for (let i = 0; i < gridSize * gridSize; i++) {
     let item = document.createElement("div");
+    item.classList.add("grid-square")
+    item.ondragstart = () => false;
     item.style.width = absSize / gridSize;
-    item.style.height = absSize / gridSize;
-    console.log(item);
+    
+    switch (i) {
+        case 0:
+            item.style.borderStyle = "none dashed dashed none";
+            break;
+        case gridSize - 1:
+            item.style.borderStyle = "none none dashed dashed";
+            break;
+        case gridSize * (gridSize - 1):
+            item.style.borderStyle = "dashed dashed none none";
+            break;
+        case gridSize * gridSize - 1:
+            item.style.borderStyle = "dashed none none dashed";
+            break;
+    } 
+
+    item.addEventListener("mousedown", (event) => {
+        event.target.style.backgroundColor = currentColor;
+    })
+    item.addEventListener("mouseover", (event) => {
+        if(event.buttons == 1)
+            event.target.style.backgroundColor = currentColor;
+    });
     grid.appendChild(item);
 }
+
